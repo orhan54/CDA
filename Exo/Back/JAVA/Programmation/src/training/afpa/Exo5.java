3f163cf54650c5acc8f57f64d4ea9d603d2ea64c
CICEK Orhan <orhancicek1985@gmail.com>
2025-07-31 22:24:14 +0200

    Exo5 terminer

--- Exo/Back/JAVA/Programmation/src/training/afpa/Exo5.java
+++ Exo/Back/JAVA/Programmation/src/training/afpa/Exo5.java
@@ -1,79 +1,68 @@
 package training.afpa;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Exo5 {
 
-    char[] operateur;
-    float operande1;
-    float operande2;
-    float resultat;
-    boolean continuer;
-
-    public float calculer(char operateur, float operande1, float operande2, float resultat) {
-
+    public float calculer(char operateur, float operande1, float operande2) {
         switch (operateur) {
             case '+':
-                resultat = operande1 + operande2;
-                break;
+                return operande1 + operande2;
             case '-':
-                resultat = operande1 - operande2;
-                break;
+                return operande1 - operande2;
             case '*':
-                resultat = operande1 * operande2;
-                break;
+                return operande1 * operande2;
             case '/':
                 if (operande2 == 0) {
+                    System.out.println("Erreur : division par zéro !");
                     return 0;
                 } else {
-                    resultat = operande1 / operande2;
+                    return operande1 / operande2;
                 }
-                break;
+            default:
+                System.out.println("Erreur : opérateur invalide.");
+                return 0;
         }
-        return resultat;
-
     }
 
     public void exProgramme() {
-        continuer = true;
+        Scanner sc = new Scanner(System.in);
+        boolean continuer = true;
 
         while (continuer) {
-            //afficher le menu et obtenir l'operation
-            System.out.println("Choisissez une opération : +, -, *, / ");
-            Scanner sc = new Scanner(System.in);
-            operateur = sc.nextLine().toCharArray();
+            System.out.println("Choisissez une opération : +, -, *, /");
+            String input = sc.nextLine();
 
-            for (int i = 0; i < operateur.length; i++) {
-                if (operateur[0] == '+' || operateur[1] == '-' || operateur[2] == '*' || operateur[3] == '/') {
-                    System.out.println("Opérateur invalide. Veuillez entrer un opérateur valide ");
-                    continuer = false;
-                }
+            // Vérifier que l'entrée est un seul caractère valide
+            if (input.length() != 1 || "+-*/".indexOf(input.charAt(0)) == -1) {
+                System.out.println("Opérateur invalide. Veuillez entrer +, -, * ou /.");
+                continue;
             }
 
-            //Saisie des opérandes
+            char operateur = input.charAt(0);
+
+            // Lire les opérandes
             System.out.println("Entrez le premier opérande (nombre réel) :");
-            operande1 = sc.nextFloat();
+            float operande1 = sc.nextFloat();
 
             System.out.println("Entrez le second opérande (nombre réel) :");
-            operande2 = sc.nextFloat();
+            float operande2 = sc.nextFloat();
 
-            //calculer le résultat
-            resultat = calculer(operateur[0], operande1, operande2, resultat);
+            sc.nextLine(); // Consomme le retour à la ligne restant
 
-            //afficher le resultat
-            System.out.println("Le resultat de " + operande1 + Arrays.toString(operateur) + operande2 + " est : " + resultat);
+            float resultat = calculer(operateur, operande1, operande2);
 
-            //Demander si l'utilisateur souhaite continuer
+            System.out.println("Le résultat de " + operande1 + " " + operateur + " " + operande2 + " est : " + resultat);
+
+            // Demander à l'utilisateur s'il veut continuer
             System.out.println("Souhaitez-vous effectuer un autre calcul ? (Oui/Non)");
-            Scanner sc2 = new Scanner(System.in);
-            String response = "Non";
-            String reponse  = sc.nextLine();
-            if(response.equals("Non")){
+            String reponse = sc.nextLine().trim();
+
+            if (reponse.equalsIgnoreCase("Non")) {
                 continuer = false;
-                sc.close();
-                System.exit(0);
+                System.out.println("Fin du programme.");
             }
         }
+        sc.close();
     }
 }
